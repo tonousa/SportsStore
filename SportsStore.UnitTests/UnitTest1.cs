7f97cd3c3f65c1d21fe7dad824478695c417cbc1
tonousa <tonousa@hotmail.com>
2015-07-12 14:56:13 -0400

    view model

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void Can_Paginate()
+        //{
+        //}
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define the HTML helper - in order to apply the extension method
+            HtmlHelper myHelper = null;
+
+            // Arrange - create pagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - setup the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>"
+                + @"<a class=""Selected"" href=""Page2"">2</a>"
+                + @"<a href=""Page3"">3</a>");
+        }
+    }
+}
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,20 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
             return View(repository.Products);
         }
     }
