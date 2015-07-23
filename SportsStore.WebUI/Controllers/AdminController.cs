ad2305ec50ad61a6b95d4986006d17a3dffc4d1c
tonousa <tonousa@hotmail.com>
2015-07-23 09:24:55 -0400

    add

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -44,5 +44,10 @@ namespace SportsStore.WebUI.Controllers
                 return View(product);
             }
         }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
     }
 }
